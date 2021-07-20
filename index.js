d904433f0a956da043d1041b91047c7e12c618ce
github username <github email address>
2021-07-19 21:34:15 -0400

    sliders working

--- index.js
+++ index.js
@@ -7,16 +7,32 @@ document.addEventListener("DOMContentLoaded", () => {
         const response = await fetch("https://bengarlock.com/api/v1/healthtracker/players/")
         const players = await response.json()
         renderPlayers(players)
+        clickHandler()
     }
 
     const renderPlayers = (data) => {
         data.map(player => {
+
+
             const playerDiv = document.createElement('div')
             playerDiv.className = "player"
             playerDiv.dataset.playerId = player.id
+
+            let meterDiv = document.createElement("div")
+            meterDiv.style.position = 'relative'
+            meterDiv.className = "meter"
+            meterDiv.innerHTML = "X"
+            meterDiv.style.left = String(Number(player.status) * 10) + "%"
+
+            playerDiv.appendChild(meterDiv)
+
             playerDiv.innerHTML = `
                 <h4>${player.name}</h4>
-                <div class="status">${player.status}</div>
+                <div class="meter" style="position: relative; left: ${String(player.status * 10) + "%"}">X</div>
+                <div>Status:</div>
+                <div class='status'>${player.status}</div>
+                <div>Goal:</div>
+                <div class="goals">${player.goal}</div>
                 <button class="decrement">-</button>
                 <button class="increment">+</button>                       
             `
@@ -40,6 +56,10 @@ document.addEventListener("DOMContentLoaded", () => {
         let statusDiv = data.querySelector('.status')
         statusDiv.innerHTML = Number(statusDiv.innerHTML) + 1
         updatePlayer(data.dataset.playerId, Number(statusDiv.innerHTML))
+        let meterDiv = data.querySelector('.meter')
+        meterDiv.style.position = 'relative'
+        meterDiv.style.left = String(Number(statusDiv.innerHTML) * 10) + "%"
+
     }
 
     const decrement = (data) => {
@@ -47,6 +67,10 @@ document.addEventListener("DOMContentLoaded", () => {
         if (Number(statusDiv.innerHTML) > 0) {
             statusDiv.innerHTML = Number(statusDiv.innerHTML) - 1
             updatePlayer(data.dataset.playerId, Number(statusDiv.innerHTML))
+            let meterDiv = data.querySelector('.meter')
+            meterDiv.style.position = 'relative'
+            meterDiv.style.left = String(Number(statusDiv.innerHTML) * 10) + "%"
+
         }
     }
 
@@ -63,16 +87,10 @@ document.addEventListener("DOMContentLoaded", () => {
             },
             body: JSON.stringify(data)
         }
-        const response = await fetch("https://bengarlock.com/api/v1/healthtracker/players/" + String(player) + '/', packet)
-        const update = await response.json()
-        console.log(update)
+        await fetch("https://bengarlock.com/api/v1/healthtracker/players/" + String(player) + '/', packet)
     }
 
-
-
-
-
     fetchPlayers()
-    clickHandler()
+
 })
 
